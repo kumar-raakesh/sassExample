3f7e21dd855df1978750659597a3ccdf13fdd6b4
rakesh <rakesh@leaguex.com>
2020-05-12 11:48:03 +0530

    added theme change func

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
-
+import "./css/style.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="showcase">
+        <div className="container">
+          <nav>
+            <h1 className="logo"> My Website</h1>
+            <ul>
+              <li><a href="#"></a> Home</li>
+              <li><a href="#"></a> About</li>
+              <li><a href="#"></a> Services</li>
+            </ul>
+          </nav>
+          <div className="showcase-content">
+            <div>
+              <h1>Make your Marketing Real</h1>
+              <p class="my-1">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                It has survived not only five centuries, but also the leap into electronic typesetting,
+                remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
+                sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
+                Aldus PageMaker including versions of Lorem Ipsum
+             </p>
+              <a href="#" class="btn-primary" onClick={() => { alert("Working") }}> JOIN</a>
+              <a href="#" class="btn-secondary" onClick={() => { alert("Playing") }}>PLAY</a>
+            </div>
+            <img
+              src="https://www.gstatic.com/webp/gallery3/5_webp_ll.png"
+            />
+          </div>
+        </div>
       </header>
     </div>
   );
